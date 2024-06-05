771ae5932fd84561281a9353c7a236aecd913240
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-06-05 18:48:15 +0300

    5%

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,13 +4,14 @@ export const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content">
-        <h1>Новинки від Narurehike</h1>
-        <p>
+        <h1 className="hero__title">Новинки від Narurehike</h1>
+        <p className="hero__description">
           Ассортимент Naturehike имеет большое разнообразие: палатки; спальные
           мешки; матрасы; гамаки; одежда для туризма; рюкзаки; походная посуда.
         </p>
-        <a href="">Перейти</a>
-        <h1>ewfwewdwedwe</h1>
+        <a className="hero__link" href="#">
+          Перейти
+        </a>
       </div>
     </section>
   );
