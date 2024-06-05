1af4411d51ddb84414cf01904cf9c7fa309f9b68
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-06-05 16:11:21 +0300

    start

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import "./Hero.scss";
+
+export const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__content">
+        <h1>Новинки від Narurehike</h1>
+        <p>
+          Ассортимент Naturehike имеет большое разнообразие: палатки; спальные
+          мешки; матрасы; гамаки; одежда для туризма; рюкзаки; походная посуда.
+        </p>
+        <a href="">Перейти</a>
+      </div>
+    </section>
+  );
+};
