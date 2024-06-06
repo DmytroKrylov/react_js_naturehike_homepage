a0273574be6997f9aadee902ca937d3782dac433
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-06-06 15:09:48 +0300

    55%

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,23 +4,29 @@ export const About = () => {
   return (
     <section className="about">
       <div className="about__content container">
-        <h2 className="about__title">Почему Naturehike</h2>
-        <p className="about__description">
-          Главная цель Naturehike — производство снаряжения, которое сделает
-          любой отдых на свежем воздухе легким и комфортным. <br /> Бренд
-          завоевал доверие покупателей со всего мира благодаря высокому качеству
-          и инновационному дизайну своих товаров для пеших прогулок, альпинизма,
-          треккинга и продолжительных походов.
-        </p>
-        <p className="about__paragraph">
-          Ближе познакомиться с Naturehike вы можете на странице о бренде.
-        </p>
-        <a className="about__link" href="#">
-          О Бренде
-        </a>
-        <div className="about__frame">
-          <span className="about__video"></span>
-          <span className="about__video1"></span>
+        <h2 className="about__title">
+          Почему <br /> Naturehike
+        </h2>
+        <div className="about__flex">
+          <div>
+            <p className="about__description">
+              Главная цель Naturehike — производство снаряжения, которое сделает
+              любой отдых на свежем воздухе легким и комфортным. <br /> Бренд
+              завоевал доверие покупателей со всего мира благодаря высокому
+              качеству и инновационному дизайну своих товаров для пеших
+              прогулок, альпинизма, треккинга и продолжительных походов.
+            </p>
+            <p className="about__paragraph">
+              Ближе познакомиться с Naturehike вы можете на странице о бренде.
+            </p>
+            <a className="about__link" href="#">
+              О Бренде
+            </a>
+          </div>
+          <div className="about__frame">
+            <span className="about__video"></span>
+            <span className="about__video1"></span>
+          </div>
         </div>
       </div>
     </section>
