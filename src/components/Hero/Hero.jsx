be5e968e7506cc137be4c1932631b4c155b78f0e
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-06-06 14:20:52 +0300

    50%

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,26 @@ export const Hero = () => {
   return (
     <section className="hero">
       <div className="hero__content container">
-        <h1 className="hero__title">Новинки від Narurehike</h1>
+        <div className="hero__header">
+          <a className="hero__logo" href="#"></a>
+          <ul className="hero__list">
+            <li className="hero__item">
+              <a className="hero__analytic" href="#"></a>
+              <span className="hero__span">0</span>
+            </li>
+            <li className="hero__item">
+              <a className="hero__like" href="#"></a>
+              <span className="hero__span">0</span>
+            </li>
+            <li className="hero__item">
+              <a className="hero__card" href="#"></a>
+              <span className="hero__span">0</span>
+            </li>
+          </ul>
+        </div>
+        <h1 className="hero__title">
+          Новинки від <br /> Narurehike
+        </h1>
         <p className="hero__description">
           Ассортимент Naturehike имеет большое разнообразие: палатки; спальные
           мешки; матрасы; гамаки; одежда для туризма; рюкзаки; походная посуда.
