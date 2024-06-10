7d3e6257623412c44b42957850873b59c67c54eb
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-06-10 15:54:41 +0300

    90%

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./Menu.scss";
 
 export const Menu = () => {
@@ -9,32 +11,44 @@ export const Menu = () => {
   };
 
   return (
-    <div className="menu">
-      <div className="menu__toggle" onClick={toggleMenu}>
-        <span>Каталог товарів</span>
-        <span className="menu__toggle-icon">{isOpen ? "▲" : "▼"}</span>
+    <section className="menu__section">
+      <div className="menu">
+        <div className="menu__toggle" onClick={toggleMenu}>
+          <span className="menu__button">Каталог товаров</span>
+        </div>
+        <ul className={`menu__list ${isOpen ? "open" : ""}`}>
+          <li className="menu__item">
+            <a className="menu__link" href="#">
+              Спальники
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#">
+              Коврики
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#">
+              Мебель
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#">
+              Рюкзаки
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#">
+              Гамаки
+            </a>
+          </li>
+          <li className="menu__item">
+            <a className="menu__link" href="#">
+              Посуда
+            </a>
+          </li>
+        </ul>
       </div>
-      <ul className={`menu__list ${isOpen ? "open" : ""}`}>
-        <li className="menu__item">
-          <a href="#">Спальники</a>
-        </li>
-        <li className="menu__item">
-          <a href="#">Коврики</a>
-        </li>
-        <li className="menu__item">
-          <a href="#">Мебель</a>
-        </li>
-        <li className="menu__item">
-          <a href="#">Рюкзаки</a>
-        </li>
-        <li className="menu__item">
-          <a href="#">Гамаки</a>
-        </li>
-        <li className="menu__item">
-          <a href="#">Посуд</a>
-        </li>
-        <div className="menu__scroll-indicator"></div>
-      </ul>
-    </div>
+    </section>
   );
 };
